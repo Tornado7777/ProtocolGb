c92c9481e917717bbee0bb5b31ba171513716852
Иван А.А <I.Andryushchenko@i-teco.ru>
2023-03-05 21:53:02 +0300

    lesson 2 wcf pumpservice

---
+++ PumpService/PumpClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using PumpClient.PumpServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+            PumpServiceClient client = new PumpServiceClient(instanceContext);
+
+            client.UpdateAndCompileScript("D:\\Users\\repository\\C_Sharp\\ProtocolsTest\\PumpService\\PumpService\\Scripts\\Sample.script");
+            
+            client.RunScript();
+
+            Console.ReadKey(true);
+            client.Close();
+        }
+    }
+}
